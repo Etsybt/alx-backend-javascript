57d7f3458d87aa1b153a3563129b573053def649
Etsybt <nouhailahmidouch2@gmail.com>
2024-08-19 16:21:41 +0000

    task1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,9 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  console.log(`Your name is: ${name}`);
+  process.stdout.write(`Your name is: ${data}`);
+});
 
-  if (!process.stdin.isTTY) {
-    console.log('This important software is now closing');
-  }
-
-  process.exit();
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
