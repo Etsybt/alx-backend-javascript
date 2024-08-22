a88dcd370db0c9d660b7a858a1ae11f2b58e8aa7
Etsybt <nouhailahmidouch2@gmail.com>
2024-08-22 21:22:36 +0000

    9-api PUSH

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -1,36 +1,58 @@
-const request = require('supertest');
-const app = require('./api');
+const { expect } = require('chai');
+const request = require('request');
+
+const serverUrl = 'http://localhost:7865';
 
 describe('Index Page', () => {
   it('should return status code 200', (done) => {
-    request(app)
-      .get('/')
-      .expect(200, done);
+    request.get(serverUrl, (error, response) => {
+      if (error) {
+        return done(error);
+      }
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
   });
 
   it('should return the correct message', (done) => {
-    request(app)
-      .get('/')
-      .expect('Welcome to the payment system', done);
+    request.get(serverUrl, (error, response, body) => {
+      if (error) {
+        return done(error);
+      }
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
   });
 });
 
 describe('Cart Page', () => {
-  it('should return status code 200 when id is a number', (done) => {
-    request(app)
-      .get('/cart/12')
-      .expect(200, done);
+  it('should return status code 200 when :id is a number', (done) => {
+    request.get(`${serverUrl}/cart/123`, (error, response) => {
+      if (error) {
+        return done(error);
+      }
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
   });
 
-  it('should return the correct message when id is a number', (done) => {
-    request(app)
-      .get('/cart/12')
-      .expect('Payment methods for cart 12', done);
+  it('should return the correct message when :id is a number', (done) => {
+    request.get(`${serverUrl}/cart/123`, (error, response, body) => {
+      if (error) {
+        return done(error);
+      }
+      expect(body).to.equal('Payment methods for cart 123');
+      done();
+    });
   });
 
-  it('should return status code 404 when id is not a number', (done) => {
-    request(app)
-      .get('/cart/hello')
-      .expect(404, done);
+  it('should return status code 404 when :id is NOT a number', (done) => {
+    request.get(`${serverUrl}/cart/abc`, (error, response) => {
+      if (error) {
+        return done(error);
+      }
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
   });
 });
