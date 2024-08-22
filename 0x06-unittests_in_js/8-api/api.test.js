47b25e212baec703f0f2475974b3b5f04cc3eccf
Etsybt <nouhailahmidouch2@gmail.com>
2024-08-22 20:39:33 +0000

    8-api PUSH

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,18 +1,9 @@
 const { expect } = require('chai');
 const request = require('request');
-const { server } = require('./api');
 
 const serverUrl = 'http://localhost:7865';
 
 describe('Index Page', () => {
-  before((done) => {
-    // Ensure the server is not already running
-    if (server.listening) {
-      return done();
-    }
-    server.listen(7865, done); // Start the server if it's not already running
-  });
-
   it('should return status code 200', (done) => {
     request.get(serverUrl, (error, response) => {
       if (error) {
@@ -31,9 +22,5 @@ describe('Index Page', () => {
       expect(body).to.equal('Welcome to the payment system');
       done();
     });
-  });
-
-  after((done) => {
-    server.close(done); // Close the server after the tests
   });
 });
